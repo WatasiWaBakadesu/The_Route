bdb9102e20cfee2251452802a690b97431af8716
NeroSecre <79558199+NeroSecre@users.noreply.github.com>
2021-05-19 17:54:54 +0545

    This is a simple mobile app that will help users to find their Route or bus to their Destination.

--- app/src/main/java/com/example/theroute/MapsActivity.java
+++ app/src/main/java/com/example/theroute/MapsActivity.java
@@ -1,9 +1,17 @@
 package com.example.theroute;
 
-import androidx.fragment.app.FragmentActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+//import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+//import android.renderscript.Sampler;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,13 +19,20 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements
+        GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener,
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean permissionDenied = false;
     private GoogleMap mMap;
     int a;
     LanLong lanLong;
+//    GetLocationAccess getLocationAccess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
@@ -38,38 +54,146 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+   /* private void getLocationAccess() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.isMyLocationEnabled() = true;
+        else
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), Location_Permission_Request);
+        }
+        override fun onRequestPermissionsResult(requestCode :Int, permission:Array<String>,
+        grantResult:
+        IntArray){
+            if (requestCode == Location_Permission_Request) {
+                if (grantResult.contains(PackageManager.PERMISSION_GRANTED)) {
+                    mMap.isMyLocationEnabled() = true;
+                } else {
+                    Toast.makeText(this, "Access Denied", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
+    }*/
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
+//        getLocationAccess =new GetLocationAccess(mMap);
         Intent intent = getIntent();
         LatLng zoomOn = new LatLng(27.7023816, 85.3282081);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zoomOn, 12));
         lanLong=new LanLong(mMap);
-        a = Integer.parseInt(intent.getStringExtra("optionMap"));
-        switch (a) {
-            case 0: lanLong.forChandragiri();
-                break;
-            case 1: lanLong.forSundarijal();
-                break;
-            case 2: lanLong.forSanga();
-                break;
-            case 3: lanLong.forRatnapark();
-                break;
-            case 4: lanLong.forChampadevi();
-                break;
-            case 5: lanLong.forKalnki();
-                break;
-            case 6: lanLong.forPilotBaba();
-                break;
-            case 7: lanLong.forDolesworMahadev();
-                break;
-            case 8: lanLong.forNagarkot();
-                break;
-            case 9: lanLong.forDhulikhel();
-                break;
-            default:
-                Toast.makeText(this, "Invalid choice", Toast.LENGTH_SHORT).show();
-                break;
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+        enableMyLocation();
+       try {
+           a = Integer.parseInt(intent.getStringExtra("optionMap"));
+
+           switch (a) {
+               case 0:
+                   lanLong.forChandragiri();
+                   break;
+               case 1:
+                   lanLong.forSundarijal();
+                   break;
+               case 2:
+                   lanLong.forSanga();
+                   break;
+               case 3:
+                   lanLong.forRatnapark();
+                   break;
+               case 4:
+                   lanLong.forChampadevi();
+                   break;
+               case 5:
+                   lanLong.forKalnki();
+                   break;
+               case 6:
+                   lanLong.forPilotBaba();
+                   break;
+               case 7:
+                   lanLong.forDolesworMahadev();
+                   break;
+               case 8:
+                   lanLong.forNagarkot();
+                   break;
+               case 9:
+                   lanLong.forDhulikhel();
+                   break;
+               default:
+                   Toast.makeText(this, "Invalid choice", Toast.LENGTH_SHORT).show();
+                   break;
+           }
+       }
+       catch (NullPointerException e){
+           Toast.makeText(this, "Access Cannot be denied", Toast.LENGTH_SHORT).show();
+
+       }
+    }
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        // [START maps_check_location_permission]
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (mMap != null) {
+                mMap.setMyLocationEnabled(true);
+            }
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
         }
+        // [END maps_check_location_permission]
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    // [START maps_check_location_permission_result]
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Permission was denied. Display an error message
+            // [START_EXCLUDE]
+            // Display the missing permission error dialog when the fragments resume.
+            permissionDenied = true;
+            // [END_EXCLUDE]
+        }
+    }
+    // [END maps_check_location_permission_result]
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (permissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            permissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 }
